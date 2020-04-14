fddfbc898d9bb2f185aeda204a97a9eac77ed1f9
Yunda Yin <hentaizephyr@gmail.com>
2020-04-14 17:59:03 +1200

    RecyclerView of items and a basic page of Viewing single Item

---
+++ PhotoAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinApp
+{
+    class PhotoAdapter : Android.Support.V7.Widget.RecyclerView.Adapter
+    {
+        public event EventHandler<int> ItemClick;
+        public Album mPhotoAlbum;
+        public PhotoAdapter(Album photoAlbum)
+        {
+            mPhotoAlbum = photoAlbum;
+        }
+        public override int ItemCount
+        {
+            get { return mPhotoAlbum.numPhoto; }
+        }
+
+        public override void OnBindViewHolder(Android.Support.V7.Widget.RecyclerView.ViewHolder holder, int position)
+        {
+            PhotoViewHolder vh = holder as PhotoViewHolder;
+            vh.Image.SetImageResource(mPhotoAlbum[position].mPhotoID);
+            vh.Caption.Text = mPhotoAlbum[position].mCaption;
+        }
+
+        public override Android.Support.V7.Widget.RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.PhotoCard, parent, false);
+            PhotoViewHolder vh = new PhotoViewHolder(itemView, OnClick);
+            return vh;
+        }
+        private void OnClick(int obj)
+        {
+            if (ItemClick != null)
+                ItemClick(this, obj);
+        }
+    }
+}
